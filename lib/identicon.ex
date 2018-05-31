987e0506e088bdff97c06469d0fce1fcf4320c17
Gregory Mendes <contato@gregorymendes.com>
2018-05-30 22:51:17 -0300

    mirror row

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,10 +4,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
-  end
-
-  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-    %Identicon.Image{image | color: {r, g, b}}
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -15,6 +12,21 @@ defmodule Identicon do
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    
+  end
+
+  def mirror_row(row) do
+    [firs, second | _tail ] = row
+    row ++ [second, firs]
   end
 
 end
