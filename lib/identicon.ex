2e5a234fb8dd31e59f7eddc8645c0967d2b80ba1
Gregory Mendes <contato@gregorymendes.com>
2018-05-31 19:42:16 -0300

    Render image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,7 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
   end
 
   def hash_input(input) do
@@ -53,6 +54,16 @@ defmodule Identicon do
       {top_left, bottom_right}
     end
     %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :edg.create(250, 250)
+    fill = :edg.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) -> 
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+    :egd.render(image)
   end
 
 end
